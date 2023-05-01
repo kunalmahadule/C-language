851f3b230ece3a75a39aa03b2d23af25f2d7268a
kunalmahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-05-01 23:49:15 +0530

    Basic Tokens and Keywords

---
+++ addv4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b;
+
+    // video 4
+    //
+    // video 5 me theory hai tokens, keywords etc.
+    //\n ye new line charactor hai
+    
+    printf("enter value a\n");
+    scanf("%d", &a);
+
+    printf("enter value b\n");
+    scanf("%d", &b);
+
+    printf("The sum is %d\n", a+b);
+    
+     return 0;
+}
