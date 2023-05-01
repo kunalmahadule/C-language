6523036c5974e5398f4a7ec558062a148c506f6e
kunalmahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-05-01 23:28:27 +0530

    Basic Operators in C language

---
+++ add2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = 8;
+    int b = 80;
+
+    // printf("a + b=%d\n", a + b);
+     printf("a / b=%d\n", a / b);
+    // printf("a - b=%d\n", a - b);
+    // printf("a * b=%d\n", a * b);
+    return 0;
+}
