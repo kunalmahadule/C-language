bca0b485d0fdb06246c84dcf37f0c79e630531a8
kunalmahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-05-06 10:11:03 +0530

    working with final variable

---
+++ feco.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# define PI 3.14
+int main(int argc, char const *argv[])
+{
+    int a = 80;
+    //  const float b = 3.3;
+    // b= 4.8;
+    // printf("%.2f \n", b);
+    printf("%.4f \t heooidjb \n", PI);
+
+    // printf("The value of a is %d and the value of b is %f \n", a , b);
+    //printf(" %7.3f  new line \n my backlash \a\a \t\tblack", b);
+
+    return 0;
+}
