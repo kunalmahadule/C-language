26af6a307beef2cddd9133de66f194a0a8ea4c06
kunalmahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-05-07 14:09:10 +0530

    Looping and Infinite loop in C

---
+++ FORLOOP.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    int i = 0, j = 0;
+    for (; i < 5; i++)
+    {
+        printf("%d %d\n", i, j);
+    }
+    return 0;
+}
+
+ //Infinity loops:-
+
+// // 1.Infinity While loop (syntax)
+// // #include <stdio.h>
+// // #include <stdlib.h>
+// // int main()
+// // {
+// //     printf("Hello World 2\n");
+
+// //     while (1)
+// //     {
+// //         printf("Hello World");
+// //     }
+
+// //     return 0;
+// // }
+
+// //2.Infinity Do-While loop (syntax)
+// // #include <stdio.h>
+// // #include <stdlib.h>
+// // int main()
+// // {
+// //     printf("Hello World\n");
+
+// //     do
+// //     {
+// //         printf("Hello World");
+// //     }while (1);
+
+// //     return 0;
+// // }
+
+// //3.Infinity For loop  (syntax)
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//     printf("uiydefuiey vbiufvuyfjhdshgf7 jhegfuygdsfuyds jfg fdsh jhgfdf j  y  bsdjhjh \n");
+//     for(;;)
+//     {
+//         printf("Hello World\n");
+//     }
+
+// }
