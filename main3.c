33c9cfad00a9d610d89fa00afbd639a6a8f768cc
kunalmahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-05-07 20:20:34 +0530

    gcc flages in terminal

---
+++ main3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{ 
+  // video 3
+   //#include<stdio.h> isko preprossing command bola jata hai
+   //int main()-yaha se program start honga
+   //coading banee hoti hai keywords,identityfiers,constant,string letral,symbol.
+   //-Wall -save-temps ye sab flags hai gcc ke
+   //(keywords-int,char,32 types hai),(identityfiers-),(constant-return 0 ka jo 
+   //0 hai vo conatant hai),(string letral-),(symbol-scanf("%d", &a)&a ye hai symbol)
+   printf("Hello World");
+   return 0;
+}
