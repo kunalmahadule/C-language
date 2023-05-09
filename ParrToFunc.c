79dc9638aa74685e353eae09ba46c2ead8926303
kunalmahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-05-09 20:37:43 +0530

    Array & Pointer as a parameter

---
+++ ParrToFunc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int func1(int arr[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("The value at %d is %d\n", i, arr[i]);
+    }
+    arr[0] = 5; //Very important point that if you change any value here, it gets reflected in main()
+    return 0;
+}
+
+void func2(int *ptr)
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("The value at %d is %d\n", i, *(ptr + i));
+    }
+    *(ptr + 2) = 343;
+}
+
+void func3(int arr[2][2])
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("The value at %d, %d is %d\n", i, j, arr[i][j]);
+        }
+    }
+}
+
+int main()
+{
+    int arr[2][2] = {{2, 23}, {11, 14}};
+    // printf("The value at index 0 is %d\n", arr[0]);
+    // func1(arr);
+    // printf("The value at index 0 is %d\n", arr[0]);
+    // func2(arr);
+    // func2(arr);
+
+    func3(arr);
+    return 0;
+}
